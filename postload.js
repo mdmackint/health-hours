870616a3d89ab7e234c05115b58b8f6d690fdb52
Martin <martindmackintosh@outlook.com>
2024-11-22 21:33:36 +0000

    add vibration on mobile

--- postload.js
+++ postload.js
@@ -17,6 +17,7 @@ document.addEventListener("mousedown", function () {
     })
 
     mouseDownTimestamp = Date.now()
+    navigator.vibrate([50,50,50,50])
 })
 
 document.addEventListener("touchstart", function () {
@@ -43,6 +44,7 @@ document.addEventListener("touchend", function () {
         localStorage.setItem("hours",hours + 1)
         hours = Number(localStorage.getItem("hours"))
         document.getElementById("numberItself").innerText = hours
+        navigator.vibrate([97,23,87,16,94,15,84,29,54,39,64,28,64,0,0,56,44,97,46,92,46,93])
     }
 })
 /*document.addEventListener("long-press", function () {
